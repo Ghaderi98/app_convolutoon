ae7242443ad23841be4ad072072e1732a65d903e
elahe <83836065+Ghaderi98@users.noreply.github.com>
2023-09-23 10:28:54 +0330

    Update app_convoltion.py

--- app_convoltion.py
+++ app_convoltion.py
@@ -11,6 +11,7 @@ import streamlit as st
 import requests
 import json
 import pandas as pd
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
